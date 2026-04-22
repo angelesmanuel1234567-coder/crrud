4f990b2df18ee5a8002ce1d14dbc7072270ee867
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 15:08:51 -0500

    Update conxion.js cambio de base de datos

--- config/conxion.js
+++ config/conxion.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'shinkansen.proxy.rlwy.net',
-    user: 'root',
-    password: 'hrcnGbOVsJfkccaMyCqNmqlIOQhWipgI',
-    database: 'railway',
-    port: 18280,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10
 });
