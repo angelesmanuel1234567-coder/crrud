e714550c39e605c9bf8bd46238020b133eddc050
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 12:33:07 -0500

    para rile

--- config/conxion.js
+++ config/conxion.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    host: 'shinkansen.proxy.rlwy.net',
+    user: 'root',
+    password: 'hrcnGbOVsJfkccaMyCqNmqlIOQhWipgI',
+    database: 'railway',
+    port: 18280,
     waitForConnections: true,
     connectionLimit: 10
 });
