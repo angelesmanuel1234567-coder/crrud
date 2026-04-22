a99bf58d41b6dfd27a9f2228f485125a3749a860
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 15:04:21 -0500

    Update conxion.jscambiando nonmbre

--- config/conxion.js
+++ config/conxion.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'shinkansen.proxy.rlwy.net',
     user: 'root',
     password: 'hrcnGbOVsJfkccaMyCqNmqlIOQhWipgI',
-    database: 'bd',
+    database: 'railway',
     port: 18280,
     waitForConnections: true,
     connectionLimit: 10
