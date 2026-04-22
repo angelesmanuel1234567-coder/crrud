75bcc00640edeef9002b0e655c0a31bc09331b35
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-04-22 14:59:33 -0500

    prueba uno cambiocontrolador par enviar mensajes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,40 +2,67 @@ const Producto = require('../models/usermodels');
 
 // vista principal
 exports.vista = async (req, res) => {
-    const productos = await Producto.getAll();
+    try {
+        const productos = await Producto.getAll();
 
-    res.render('productos', { 
-        productos,
-        editar: null   // SIEMPRE EXISTE
-    });
+        res.render('productos', { 
+            productos,
+            editar: null
+        });
+
+    } catch (error) {
+        console.error("ERROR EN VISTA:", error);
+        res.status(500).send("Error al cargar productos");
+    }
 };
 
 // crear
 exports.guardar = async (req, res) => {
-    await Producto.create(req.body);
-    res.redirect('/productos');
+    try {
+        await Producto.create(req.body);
+        res.redirect('/productos');
+    } catch (error) {
+        console.error("ERROR AL GUARDAR:", error);
+        res.send("Error al guardar");
+    }
 };
 
 // eliminar
 exports.eliminar = async (req, res) => {
-    await Producto.delete(req.params.id);
-    res.redirect('/productos');
+    try {
+        await Producto.delete(req.params.id);
+        res.redirect('/productos');
+    } catch (error) {
+        console.error("ERROR AL ELIMINAR:", error);
+        res.send("Error al eliminar");
+    }
 };
 
 // actualizar
 exports.editar = async (req, res) => {
-    const id = req.params.id;
-    await Producto.update(id, req.body);
-    res.redirect('/productos');
+    try {
+        const id = req.params.id;
+        await Producto.update(id, req.body);
+        res.redirect('/productos');
+    } catch (error) {
+        console.error("ERROR AL EDITAR:", error);
+        res.send("Error al editar");
+    }
 };
 
 // cargar formulario editar
 exports.formEditar = async (req, res) => {
-    const producto = await Producto.getById(req.params.id);
-    const productos = await Producto.getAll();
+    try {
+        const producto = await Producto.getById(req.params.id);
+        const productos = await Producto.getAll();
 
-    res.render('productos', { 
-        productos,
-        editar: producto || null   // 👈 seguridad extra
-    });
+        res.render('productos', { 
+            productos,
+            editar: producto || null
+        });
+
+    } catch (error) {
+        console.error("ERROR FORM EDITAR:", error);
+        res.send("Error al editar formulario");
+    }
 };
